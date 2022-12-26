4554dc227c01f4aa72918c94cde815e648461143
Himanshu <mehra3228@gmail.com>
2022-12-26 23:06:50 +0530

    commit

--- 75_ProtoType.js
+++ 75_ProtoType.js
@@ -9,6 +9,17 @@ let p = {
     alert("run");
   },
 };
+let b={
+    age:'25',
+    vechilName:'ford Mustang',
+}
+
 //setting prototype
 a.__proto__ = p;
+p.__proto__=b;
+//first run check in object itself if not found than 
+//find in its protoype until not found go on to proto,
+//if at the end not found return undefined
 a.run();
+console.log(a.age);
+
