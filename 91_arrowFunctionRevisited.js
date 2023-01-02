2e0ef672a125c941b738d92b0d25f90e6941a5ba
Himanshu <mehra3228@gmail.com>
2023-01-02 21:13:41 +0530

    comit

--- 91_arrowFunctionRevisited.js
+++ 91_arrowFunctionRevisited.js
@@ -59,6 +59,6 @@ x.showDetails();
 //It keeps checking enclosing scopes until it finds the variable or reaches global scope.
 // Now, function definitions that are not arrow functions define this for you, implicitly.
 // Thus, they will never check an enclosing scope when you try to use this in their scope
-// (because they find it in their own scope!). 
+// (because they find it in their own scope!).
 //Arrow functions do NOT define their own this, 
 //so they go to the enclosing scope and look for it just as they would with any variable you try to use in their scope.
