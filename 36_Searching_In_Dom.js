dab5902e87f56210efa926e1bbb86fe11f1c06ce
Himanshu <mehra3228@gmail.com>
2023-03-16 23:17:57 +0530

    ds

--- 36_Searching_In_Dom.js
+++ 36_Searching_In_Dom.js
@@ -5,13 +5,13 @@ cTitle.style.color="red";
 //query selector
 //node list
 let allTitle=document.querySelectorAll(".card-title");
-//node list wiil be returned
+//node list wiil be returned.
 console.log(allTitle);
 allTitle[0].style.color="green";
 allTitle[1].style.color="blue";
 allTitle[2].style.color="red";
 
-// it will select the first element of the css selctor
+// it will select the first element of the css selctor.
 //document.querySelectorAll(css)[0] = querySelector(css);
 document.querySelector(".this").style.color="red";
 
