6506649051482a6ced1f404d9710624cb5f6241e
Himanshu <mehra3228@gmail.com>
2023-03-17 13:40:53 +0530

    cmt

--- 36_Searching_In_Dom.js
+++ 36_Searching_In_Dom.js
@@ -13,11 +13,13 @@ allTitle[2].style.color="red";
 
 // it will select the first element of the css selctor.
 //document.querySelectorAll(css)[0] = querySelector(css);
+
 document.querySelector(".this").style.color="red";
 
 
 //document.getElementsByTagName-> return the the given tag in collection of all 
 //shows all a elements
+
 console.log(document.getElementsByTagName("a"));
 console.log(document.querySelector(".card").getElementsByTagName("a"));
 //we can go like this also
